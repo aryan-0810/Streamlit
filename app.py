bf7e93f4188f57b2d9f2693a4351b5d3e4a1d454
aryan-0810 <aryanraj.pandya3357@gmail.com>
2026-08-26 23:20:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.write("Welcome to insta stats")
+
+
+# 1. Zomato — Restaurant Analytics Dashboard
+
+# Use case: Streamlit could be used to create an interactive dashboard showing restaurant ratings, orders, revenue, customer preferences, and delivery performance for data analysis.
+
+# 2. Flipkart — Product Performance Dashboard
+
+# Use case: Streamlit could help analysts build a dashboard to monitor product sales, prices, ratings, customer reviews, and inventory levels interactively.
+
+# 3. Spotify — Music Recommendation System
+
+# Use case: Streamlit could provide a simple web interface where users select a song and receive recommendations for similar songs based on a machine-learning model.
